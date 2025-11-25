04bf39fee09843244d4cd0f87608b0dae4c6ea0b
Thais Campos de Carvalho <thais.ccarvalho@sp.senac.br>
2025-11-25 20:19:21 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//import { createServer} from 'node:http'
+
+//const server = createServer((request, response)=>{
+  //  console.log('Olá')
+
+    //return response.end()
+//})
+
+//server.listen(3333)
