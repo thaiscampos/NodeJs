21bbf2521abdfb8d3cb11f2f180caf49b7c2693f
Thais Campos de Carvalho <thais.ccarvalho@sp.senac.br>
2025-11-25 21:38:43 -0300

    update20

--- server.js
+++ server.js
@@ -7,3 +7,31 @@
 //})
 
 //server.listen(3333)
+
+
+
+
+// ESM
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+})
+
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send(console.log('Thais'))
+})
+fastify.get('/Home', function (request, reply) {
+  reply.send(console.log('Home'))
+})
+
+// Run the server!
+fastify.listen({ port: 3333 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
